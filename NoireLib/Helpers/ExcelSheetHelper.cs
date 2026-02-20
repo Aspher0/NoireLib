2d6546de42e8da993d5f89aebba0d03a1d5c4514
Aspher0 <154540490+Aspher0@users.noreply.github.com>
2026-02-20 15:46:06 +0100

    Updated ExcelSheetHelper

--- NoireLib/Helpers/ExcelSheetHelper.cs
+++ NoireLib/Helpers/ExcelSheetHelper.cs
@@ -1,7 +1,10 @@
 using Dalamud.Game;
 using Lumina.Excel;
+using NoireLib.Helpers.ObjectExtensions;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NoireLib.Helpers;
 
@@ -32,7 +35,7 @@ public static class ExcelSheetHelper
     /// </summary>
     /// <typeparam name="T">The type of the Excel row.</typeparam>
     /// <param name="lang">The client language. If null, uses the current client language.</param>
-    /// <returns>The Excel sheet of type T for the specified language, or null if not found.</returns>
+    /// <returns>The Excel sheet of type <typeparamref name="T"/> for the specified language, or null if not found.</returns>
     public static ExcelSheet<T>? GetSheet<T>(ClientLanguage? lang = null) where T : struct, IExcelRow<T>
     {
         var language = lang.HasValue ? lang.Value : NoireService.ClientState.ClientLanguage;
@@ -43,5 +46,58 @@ public static class ExcelSheetHelper
         // Lazy load if not found
         LoadSheets<T>();
         return Sheets.TryGetValue((typeof(T), language), out sheet) ? sheet as ExcelSheet<T> : null;
+    }
+
+    /// <summary>
+    /// Retrieves a row of data from the specified Excel sheet by its unique identifier.
+    /// </summary>
+    /// <typeparam name="T">The type of the Excel row.</typeparam>
+    /// <param name="rowId">The unique identifier of the row to retrieve.</param>
+    /// <param name="lang">An optional client language to use when retrieving the row. If not specified, the default language is used.</param>
+    /// <returns>An instance of type <typeparamref name="T"/> representing the requested row if found; otherwise, null.</returns>
+    public static T? GetRow<T>(uint rowId, ClientLanguage? lang = null) where T : struct, IExcelRow<T>
+    {
+        var sheet = GetSheet<T>(lang);
+
+        if (sheet?.TryGetRow(rowId, out var row) ?? false)
+            return row;
+
+        return null;
+    }
+
+    /// <summary>
+    /// Finds the first row in the specified Excel sheet that matches the given predicate.
+    /// </summary>
+    /// <typeparam name="T">The type of the Excel row.</typeparam>
+    /// <param name="predicate">A function to test each row for a condition.</param>
+    /// <param name="lang">An optional client language to use when retrieving the row. If not specified, the default language is used.</param>
+    /// <returns>An instance of type <typeparamref name="T"/> representing the first matching row if found; otherwise, null.</returns>
+    public static T? FindRow<T>(Func<T, bool> predicate, ClientLanguage? lang = null) where T : struct, IExcelRow<T>
+    {
+        var sheet = GetSheet<T>(lang);
+
+        if (sheet == null || predicate == null)
+            return null;
+
+        var row = sheet.FirstOrDefault(predicate);
+
+        return row.IsDefault() ? null : row;
+    }
+
+    /// <summary>
+    /// Finds all rows in the specified Excel sheet that match the given predicate.
+    /// </summary>
+    /// <typeparam name="T">The type of the Excel row.</typeparam>
+    /// <param name="predicate">A function to test each row for a condition.</param>
+    /// <param name="lang">An optional client language to use when retrieving the rows. If not specified, the default language is used.</param>
+    /// <returns>An IEnumerable of <typeparamref name="T"/> representing all matching rows in the sheet, or an empty collection if no matches are found.</returns>
+    public static IEnumerable<T> FindRows<T>(Func<T, bool> predicate, ClientLanguage? lang = null) where T : struct, IExcelRow<T>
+    {
+        var sheet = GetSheet<T>(lang);
+
+        if (sheet == null || predicate == null)
+            return Enumerable.Empty<T>();
+
+        return sheet.Where(predicate);
     }
 }
