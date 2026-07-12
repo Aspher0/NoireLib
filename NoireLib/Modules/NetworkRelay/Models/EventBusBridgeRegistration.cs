12230a95eb621750cf23e338f8f1dcb63fa6653c
Aspher0 <154540490+Aspher0@users.noreply.github.com>
2026-07-12 23:33:59 +0200

    Remove NetworkRelay

--- NoireLib/Modules/NetworkRelay/Models/EventBusBridgeRegistration.cs
+++
@@ -1,9 +0,0 @@
-using NoireLib.EventBus;
-
-namespace NoireLib.NetworkRelay;
-
-internal sealed class EventBusBridgeRegistration(string? key, EventSubscriptionToken token)
-{
-    public string? Key { get; } = key;
-    public EventSubscriptionToken Token { get; } = token;
-}
